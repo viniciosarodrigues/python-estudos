25f4e109847f05b3af5b29a63146565b85ed2aaa
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2018-12-06 23:10:51 -0300

    Exercício 3 (Somando dois números)

---
+++ ex002.py
@@ -0,0 +1,5 @@
+print('======================= Exercício 3 (Somando dois números) ==========================')
+n1 = int(input('Informe o primeiro número: '))
+n2 = int(input('Informe o segundo número: '))
+soma = n1 + n2
+print('Cálculo: {0} + {1} = {2}'.format(n1, n2, soma))
