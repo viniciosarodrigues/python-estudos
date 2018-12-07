c8e97e55a5954dc7e41b042c6a0cd9531b5592b4
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2018-12-06 23:10:23 -0300

    Exercício 2 (Interagindo com o usuário usando Input)

---
+++ ex001.py
@@ -0,0 +1,3 @@
+print('======================= Exercício 2 (Interagindo com o usuário) ==========================')
+nome = input('Qual é o seu nome?')
+print('Olá,', nome + ', seja bem-vindo! É um prazer te conhecer.')
