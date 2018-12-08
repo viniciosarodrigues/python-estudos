5923db22c53bb783eb14f22c9b90ddba2443580f
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2018-12-08 13:12:06 -0300

    Exercício 14 (Conversor de temperatura)

---
+++ ex013.py
@@ -0,0 +1,10 @@
+print('======== Exercício 14 (Conversor de temperatura ========')
+
+conversorDe = input('O que você deseja fazer?\n1 - Conversor de Celsius para Fahrenheit\n2 - Conversor de Fahrenheit para Celsius\nOpção: ')
+print('==========================================================')
+if(conversorDe == '1'):
+    temperatura = float(input('Informe a temperatura em Celsius: '))
+    print('A temperatura de {} ºC equivale a {} ºF'.format(temperatura, (temperatura * (9 / 5)) + 32))
+else:
+    temperatura = float(input('Informe a temperatura em Fahrenheit: '))
+    print('A temperatura de {} ºF equivale a {} ºC'.format(temperatura, (temperatura - 32) * (5 / 9)))
