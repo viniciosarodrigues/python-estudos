fb7361239d2f1e824cc8b477550f7fddbbdefd95
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2018-12-08 10:35:32 -0300

    Exercício 7 (Calcula centímetros e Milímetros)

---
+++ ex007.py
@@ -0,0 +1,5 @@
+print('======== Exercício 08 (Calcula  valores em centímetros e milímetros ========')
+
+metros = float(input('Informe a quantidade em metros: '))
+print('O valor de {}m em centímeros é de {} e em milímetros é de {}'.format(metros, (metros*100), (metros*1000)))
+
