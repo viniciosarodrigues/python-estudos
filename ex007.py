3a73eed4b80e34a2824fac716cbd9e0e7537a709
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2018-12-08 10:36:42 -0300

    Exercício 8 (Tabuada)

--- ex007.py
+++ ex007.py
@@ -2,4 +2,3 @@ print('======== Exercício 08 (Calcula  valores em centímetros e milímetros ==
 
 metros = float(input('Informe a quantidade em metros: '))
 print('O valor de {}m em centímeros é de {} e em milímetros é de {}'.format(metros, (metros*100), (metros*1000)))
-
