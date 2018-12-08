4c332185229fbe18f309980e377f830caf760c3e
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2018-12-08 10:43:04 -0300

    Exercício 10 (Conversão de Real em Dólares)

---
+++ ex009.py
@@ -0,0 +1,7 @@
+print('======== Exercício 10 (Calcula  dólares ========')
+
+reais = float(input('Informe os valores em reais: R$ '))
+
+dolares = float(input('Informe o valor do dólar hoje: US$ '))
+
+print('Com R$ {} você pode comprar US$ {}'.format(reais, reais / dolares))
