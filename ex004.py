a8cf8a4cd6898861b98f29b6d0f0a115aa5ba839
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2018-12-07 22:57:50 -0300

    Anterior e posterior

--- ex004.py
+++ ex004.py
@@ -1,5 +1,12 @@
-print('======================= Exercício 1 (Operadores Aritméticos) ==========================')
+print('============== Operadores numéricos ===============')
 
-n1 = int(input('Informe o primeiro nome' ? '')
-s
-i2 = int(input(' Informe o segundo nodo'))
+n1 = int(input('Informe um valor: '))
+n2 = int(input('Informe um outro valor: '))
+
+soma = n1 + n2
+multi = n1 * n2
+divis = n1 / n2
+div = n1 // n2
+elev = n1 ** n2
+print('A soma é {}, o produto é {} e a divisão é {}'.format(soma, multi, divis))
+print('A divisão inteira é {} e a potência de {} a {} é {}'.format(div, n1, n2, elev))
---
+++ ex005.py
@@ -0,0 +1,6 @@
+print('============== Exercício 005 (Posterior e Anterior ===============')
+
+numero = int(input('Informe um número'))
+
+print('O número anterior ao {} é {}'.format(numero, numero - 1))
+print('O número posterior ao {} é {}'.format(numero, numero + 1))
