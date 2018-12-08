a329c6c36656b4037b2b3c4b5c76e71abcae6d87
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2018-12-07 22:45:20 -0300

    Operadores Numéricos

---
+++ ex004.py
@@ -0,0 +1,5 @@
+print('======================= Exercício 1 (Operadores Aritméticos) ==========================')
+
+n1 = int(input('Informe o primeiro nome' ? '')
+s
+i2 = int(input(' Informe o segundo nodo'))
