1ea6c5f24f6c3f9da5da5ae14dca90e49c6eb1c0
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2018-12-07 23:03:28 -0300

    Exercício 006 (Calcula Média)

---
+++ ex006.py
@@ -0,0 +1,6 @@
+print('============== Exercício 006 (Calcula média) ===============')
+
+nota1 = float(input('Informe a primeira nota: '))
+nota2 = float(input('Informe a segunda nota: '))
+
+print('A média entre {} e {} é {}'.format(nota1, nota2, (nota1+nota2)/2))
