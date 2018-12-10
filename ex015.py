46a9588829ef74d6b00476abaaef3dff5bc95fda
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2018-12-09 23:17:45 -0300

    Exercício 16 (Trabalhando com importação de módulos)

---
+++ ex015.py
@@ -0,0 +1,6 @@
+import math
+print('======== Exercício 16 (Trabalhando com Import) ========')
+
+valor = float(input('Informe um valor: '))
+
+print(' A posição inteira de {} é {}'.format(valor, math.trunc(valor)))
